f4450f80998ae262850801133aa28f4e619b7124
Nadim Absi <absinad1@gmail.com>
2022-11-28 14:19:13 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const tasks = document.getElementById(`tasks`)
+  const button = document.getElementById(`button`)
+  const priority = document.getElementById(`priority`)
+  const sort = document.getElementById(`sort`)
+
+  button.addEventListener((`click`) , (e)=>{
+    e.preventDefault()
+    const toDo = document.getElementById(`new-task-description`).value
+    const newElmenet = document.createElement("li")
+    newElmenet.innerHTML=`${toDo}`
+   
+    newElmenet.style.color=(`${priority.value}`)
+
+
+
+    tasks.appendChild(newElmenet)
+    debugger;
+  })
+
+
+
+  const remover = document.getElementById(`delete`)
+  remover.addEventListener(`click`,(e)=>{
+    e.preventDefault()
+    tasks.innerHTML=``})
 });
